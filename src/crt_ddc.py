58cc1eea12f588cc63cff910600112d9516d6633
agfa <agfa.0mc@gmail.com>
2025-01-06 01:50:58 -0500

    cleanup after new logic from ooda

--- src/crt_ddc.py
+++ src/crt_ddc.py
@@ -109,139 +109,128 @@ def main():
         "0xff": "unspecified",
     }
 
-    class Dong_button():
-        def __init__(self, parent, displaynum):
-            self.displaynum = displaynum
+    class dong_button():
+        def __init__(self, parent, monitor):
             self.parent = parent
-            
+            self.monitorobj = monitor
+
         def make_button(self):
-            def donger():
-                for i, monitor in enumerate(monitors):
-                    with monitor:
-                        if i == self.displaynum:
-                            while True:
-                                try:
-                                    monitor.vcp.set_vcp_feature(code=1, value=1)
-                                    showinfo(title='Notice', message='Dong !')
-                                except:
-                                    pass
-                                else:
-                                    break
-            self.button = ttk.Button(self.parent, text="Degauss", command=donger, takefocus=False)
+            def dong():
+                while True:
+                    with self.monitorobj:
+                        try:
+                            self.monitorobj.vcp.set_vcp_feature(code=1, value=1)
+                            showinfo(title='Notice', message='Dong !')
+                        except:
+                            pass
+                        else:
+                            break
+            self.button = ttk.Button(self.parent, text="Degauss", command=dong, takefocus=False)
+            self.button.pack(side=LEFT, expand=YES, padx=padmedium)
+
+    class code_refresher():
+        def __init__(self, parent, adjustersdict, displayname):
+            self.parent = parent
+            self.adjustersdict = adjustersdict
+            self.displayname = displayname
+
+        def make_button(self):
+            def refresh():
+                print("\nNow refreshing values from monitor....")
+                for code in self.adjustersdict.keys():
+                    with self.adjustersdict[code].monitorobj:
+                        new_value = self.adjustersdict[code].monitorobj.vcp.get_vcp_feature(code=int(code, 16))[0]
+                    self.adjustersdict[code].slider.set(new_value)
+                print("Values refreshed successfully")
+                showinfo(title="Notice", message="Values refreshed for "+self.displayname)
+            self.button = ttk.Button(self.parent, text="Refresh", command=refresh, takefocus=False)
             self.button.pack(side=LEFT, expand=YES, padx=padmedium)
 
     class code_saver:
-        def __init__(self, parent, displaynum, displayname):
+        def __init__(self, parent, adjustersdict, displayname):
             self.parent = parent
-            self.displaynum = displaynum
+            self.adjustersdict = adjustersdict
             self.displayname = displayname
+
         def make_button(self):    
-            def saver():
-                filename = asksaveasfilename(initialdir="~",
-                                                        confirmoverwrite=True, 
-                                                        initialfile=self.displayname+" profile", 
-                                                        defaultextension=".json", 
-                                                        filetypes=[('json files', '*.json'), ('All Files', '**')])
+            def save():
+                filename = asksaveasfilename(
+                                            initialdir="~",
+                                            confirmoverwrite=True, 
+                                            initialfile=self.displayname+" profile", 
+                                            defaultextension=".json", 
+                                            filetypes=[('json files', '*.json'), ('All Files', '**')]
+                                            )
                 if filename:
-                    for i, monitor in enumerate(monitors):
-                        with monitor:
-                            if i == self.displaynum:
-                                print("\nSaving values to file.....\n")
-                                saved_vcp_codes = {}
-                                for code in vcp_codes.keys():
-                                    try:
-                                        if monitor.vcp.get_vcp_feature(code=int(code, 16))[1] > 127:
-                                            code_value = monitor.vcp.get_vcp_feature(code=int(code, 16))[0]
-                                            saved_vcp_codes[code] = code_value
-                                            print("Saving current value:", code_value, "for", vcp_codes[code],)
-                                    except:
-                                        pass
-                                if len(saved_vcp_codes) != 0:
-                                    with open(filename, "w") as vcp_file:
-                                        vcp_file.write(json.dumps(saved_vcp_codes, indent=3))
-                                    print("\nSaving complete for monitor", str(self.displaynum)+".....\n")
-                                    showinfo(title="Notice", message="Values saved to file:\n\n"+filename)
-            self.button = ttk.Button(self.parent, text="Save profile", command=saver, takefocus=False)
+                    print("\nSaving values to file.....\n")
+                    saved_vcp_codes = {}
+                    for code in self.adjustersdict.keys():
+                        with self.adjustersdict[code].monitorobj:
+                            saved_vcp_codes[code] = self.adjustersdict[code].monitorobj.vcp.get_vcp_feature(code=int(code, 16))[0]
+                            print("Saving current value:", saved_vcp_codes[code], "for", vcp_codes[code],)
+                    if len(saved_vcp_codes) != 0:
+                        with open(filename, "w") as vcp_file:
+                            vcp_file.write(json.dumps(saved_vcp_codes, indent=3))
+                        print("\nSaving complete for", str(self.displayname)+".....\n")
+                        showinfo(title="Notice", message="Values saved to file:\n\n"+filename)
+            self.button = ttk.Button(self.parent, text="Save profile", command=save, takefocus=False)
             self.button.pack(side=LEFT, expand=YES, padx=padmedium)
 
     class code_loader:
-        def __init__(self, parent, displaynum, adjustersdict):
+        def __init__(self, parent, adjustersdict, displayname):
             self.parent = parent
-            self.displaynum = displaynum
             self.adjustersdict = adjustersdict
+            self.displayname = displayname
+
         def make_button(self):
-            def loader():
+            def load():
                 filename = askopenfilename(initialdir="~")
                 if filename:
                     print("\nNow loading values from file....\n")
                     with open(filename, "r") as vcp_file:
                         saved_vcp_codes = json.loads(vcp_file.read())        
-                        for code, value in saved_vcp_codes.items():
-                            print("Loading value:", value, "for", vcp_codes[code])
-                            self.adjustersdict[code].slider.set(value)
-                            self.adjustersdict[code].value.set(int(float(value)))
+                        for code, saved_value in saved_vcp_codes.items():
+                            print("Loading value:", saved_value, "for", vcp_codes[code])
+                            self.adjustersdict[code].slider.set(saved_value)
+                    print("\nValues loaded for monitor", str(self.displayname)+".....\n")
                     print("\nValues loaded successfully")
                     showinfo(title="Notice", message="Values loaded from file!")
-            self.button = ttk.Button(self.parent, text="Load profile", command=loader, takefocus=False)
-            self.button.pack(side=LEFT, expand=YES, padx=padmedium)
-
-    class code_refresher():
-        def __init__(self, parent, displaynum, displayname, adjustersdict):
-            self.parent = parent
-            self.displaynum = displaynum
-            self.displayname = displayname
-            self.adjustersdict = adjustersdict
-            
-        def make_button(self):
-            def refresher():
-                for i, monitor in enumerate(monitors):
-                    with monitor:
-                        if i == self.displaynum:
-                            print("\nNow refreshing values from monitor....")
-                            for code in self.adjustersdict.keys():
-                                value = monitor.vcp.get_vcp_feature(code=int(code, 16))[0]
-                                self.adjustersdict[code].slider.set(value)
-                                self.adjustersdict[code].value.set(int(float(value)))
-                print("Values refreshed successfully")
-                showinfo(title="Notice", message="Values refreshed for "+self.displayname)
-            self.button = ttk.Button(self.parent, text="Refresh", command=refresher, takefocus=False)
+            self.button = ttk.Button(self.parent, text="Load profile", command=load, takefocus=False)
             self.button.pack(side=LEFT, expand=YES, padx=padmedium)
 
     class code_applier():
-        def __init__(self, parent, displaynum, displayname):
-            self.displaynum = displaynum
+        def __init__(self, parent, monitor, displayname):
             self.parent = parent
+            self.monitorobj = monitor
             self.displayname = displayname
 
         def make_button(self):
             def apply():
-                for i, monitor in enumerate(monitors):
-                    with monitor:
-                        if i == self.displaynum:
-                            while True:
-                                try:
-                                    monitor.vcp.save_current_settings()
-                                    showinfo(title='Notice', message='Values applied to '+self.displayname)
-                                except:
-                                    pass
-                                else:
-                                    break
+                with self.monitorobj:
+                    while True:
+                        try:
+                            self.monitorobj.vcp.save_current_settings()
+                            showinfo(title='Notice', message='Values applied to '+self.displayname)
+                        except:
+                            pass
+                        else:
+                            break
             self.button = ttk.Button(self.parent, text="Apply", command=apply, takefocus=False)
             self.button.pack(side=LEFT, expand=YES, padx=padmedium)
 
     class adjustment_box:
-        def __init__(self, crtfeature, parent, monitor, code, index):
+        def __init__(self, crtcodevalues, parent, monitor, code, index):
             self.parent = parent
             self.code = code
             self.monitorobj = monitor
             self.index = index
-            self.feature = crtfeature
-            self.value = IntVar(value=feature[0])
+            self.codevalues = crtcodevalues
+            self.boxvalue = IntVar(value=self.codevalues[0])
             self.label = ttk.Label(self.parent, text=vcp_codes[self.code])
             self.label.grid(row=self.index, column=3, padx=padlarge, sticky=W)
 
         def new_radiobutton(self):
-            self.value.set(self.value.get()+1)
+            self.boxvalue.set(self.boxvalue.get()+1)
             self.radiobox = ttk.Frame(self.parent)
             print("Creating buttons for", vcp_codes[self.code])
             self.radiobox.grid(row=self.index, column=4, sticky=EW, padx=padXL)
@@ -249,30 +238,30 @@ def main():
                 with self.monitorobj:
                     while True:
                         try:
-                            self.monitorobj.vcp.set_vcp_feature(code=int(self.code, 16), value=self.value.get()-1)
+                            self.monitorobj.vcp.set_vcp_feature(code=int(self.code, 16), value=self.boxvalue.get()-1)
                         except:
                             pass
                         else:
                             break
 
-            for option in range(1, self.feature[1]+2, 1):  
-                self.radio = ttk.Radiobutton(self.radiobox, text=(option), value=option, variable=self.value, command=radio_button_pressed)
+            for option in range(1, self.codevalues[1]+2, 1):  
+                self.radio = ttk.Radiobutton(self.radiobox, text=(option), value=option, variable=self.boxvalue, command=radio_button_pressed)
                 self.radio.pack(side=LEFT)
 
         def new_sliderbox(self):
             def field_entered(event):
-                newentry = self.value.get()
+                newentry = self.boxvalue.get()
                 if int(newentry) > 255:
                     self.slider.set(255)
                 else:
                     self.slider.set(int(newentry))
 
             def slider_changed(value):
-                self.value.set(int(float(value)))
+                self.boxvalue.set(int(float(value)))
                 with self.monitorobj:
                     while True:
                         try:
-                            self.monitorobj.vcp.set_vcp_feature(code=int(self.code, 16), value=int(self.value.get()))
+                            self.monitorobj.vcp.set_vcp_feature(code=int(self.code, 16), value=int(self.boxvalue.get()))
                         except:
                             pass
                         else:
@@ -280,28 +269,26 @@ def main():
                                 
             def buttondown():
                 self.downone.state(["disabled"])
-                minusone = self.slider.get()-1
-                self.slider.set(minusone)
+                self.slider.set(self.boxvalue.get()-1)
                 self.downone.state(["!disabled"])
 
             def buttonup():
                 self.upone.state(["disabled"])
-                plusone = self.slider.get()+1
-                self.slider.set(plusone)
+                self.slider.set(self.boxvalue.get()+1)
                 self.upone.state(["!disabled"])
 
-            self.slidervalue = ttk.Entry(self.parent, width=4, justify=CENTER, textvariable=self.value)
+            self.slidervalue = ttk.Entry(self.parent, width=4, justify=CENTER, textvariable=self.boxvalue)
             self.slidervalue.bind("<Return>", field_entered)
             self.slidervalue.config(takefocus=0)
             self.downone = ttk.Button(self.parent, image=leftarrowimg, command=buttondown, takefocus=False)
             self.upone = ttk.Button(self.parent,image=rightarrowimg, command=buttonup, takefocus=False)
-            print("Creating slider with value", self.value.get(), "for", vcp_codes[self.code])
-            self.slider = ttk.Scale(self.parent, from_=0, to=self.feature[1], length=sliderlength, value=self.feature[0], command=slider_changed)
+            print("Creating slider with value", self.boxvalue.get(), "for", vcp_codes[self.code])
+            self.slider = ttk.Scale(self.parent, from_=0, to=self.codevalues[1], length=sliderlength, value=self.codevalues[0], command=slider_changed)
             self.slider.grid(row=self.index, column=4, padx=padXL)
             self.downone.grid(row=self.index, column=0, padx=padsmall, pady=padsmall)
             self.slidervalue.grid(row=self.index, column=1, padx=padmedium)
             self.upone.grid(row=self.index, column=2, padx=padsmall, pady=padsmall)
-   
+
     monitors = get_monitors()
     monitornum = len(monitors)
     loading = Toplevel()
@@ -379,11 +366,10 @@ def main():
                     for code in vcp_codes.keys():
                         progress.update()
                         try:
-                            feature = monitor.vcp.get_vcp_feature(code=int(code, 16))
-                            codemax = feature[1]
-                            if codemax > 0 and codemax < 256:
-                                adjuster = adjustment_box(feature, noteframe, monitor, code, adjusterindex)
-                                if codemax > 16:
+                            codevalues = monitor.vcp.get_vcp_feature(code=int(code, 16))
+                            if codevalues[1] > 0 and codevalues[1] < 256:
+                                adjuster = adjustment_box(codevalues, noteframe, monitor, code, adjusterindex)
+                                if codevalues[1] > 16:
                                     adjuster.new_sliderbox()
                                     adjusterindex += 1
                                     adjustersdict[code] = adjuster
@@ -398,15 +384,15 @@ def main():
                         print("\nHigh number of adjustments configured, activating vertical resizability...")
                     buttonframe = ttk.Frame(noteframe, borderwidth=2, relief=SUNKEN)
                     buttonframe.grid(row=adjusterindex+1, column=0, columnspan=5, ipadx=padlarge, ipady=padlarge, pady=padXL)
-                    dong = Dong_button(buttonframe, i)
-                    dong.make_button()
-                    refresh = code_refresher(buttonframe, i, model, adjustersdict)
-                    refresh.make_button()
-                    saver = code_saver(buttonframe, i, model)
+                    donger = dong_button(buttonframe, monitor)
+                    donger.make_button()
+                    refresher = code_refresher(buttonframe, adjustersdict, model)
+                    refresher.make_button()
+                    saver = code_saver(buttonframe, adjustersdict, model)
                     saver.make_button()
-                    loader = code_loader(buttonframe, i, adjustersdict)
+                    loader = code_loader(buttonframe, adjustersdict, model)
                     loader.make_button()
-                    applier = code_applier(buttonframe, i, model)
+                    applier = code_applier(buttonframe, monitor, model)
                     applier.make_button()
                 noteframe.columnconfigure(3, weight=1)
         loading.destroy()
